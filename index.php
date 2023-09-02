0996562d898dd3d96dd75d13da8b3c3ab2cb8e3c
Pratik Prakash <98465129+Singhpratik1@users.noreply.github.com>
2023-09-02 19:18:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Play&family=Roboto+Slab:wght@300&family=Source+Sans+Pro:wght@300&family=Varela&display=swap" rel="stylesheet">
+    
+    
+
+    <title>Pratik Prakash</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="up">
+            <div class="title1"><span><span style="color:rgba(89, 226, 10, 0.949)">P</span>ratik <span style="color:rgba(89, 226, 10, 0.949)">P</span>rakash</span></div>
+            <div class="menu1"><i onclick="openmenu()" class="fa fa-bars"></i></div>
+        </div>
+        <div id="sidebar">
+            <div>
+                <span></span>
+            </div>
+            
+            <ul class="n">
+                <i onclick="closemenu()" class="fa fa-times" id="cross"></i>
+                <div class="mini">
+                    <img src="pratik.png" alt="Avatar">
+                </div>
+                <li><a href="#home" class="active"><i class="fa fa-home"></i>Home</a></li>
+                <li><a href="#about" class="active"><i class="fa fa-file-text-o"></i>AboutMe</a></li>
+                <li><a href="#service" class="active"><i class="fa fa-cogs"></i>Services</a></li>
+                
+                <li><a href="#certifacate" class="active"><i class="fa fa-wpforms"></i>Certifacate</a></li>
+                <li><a href="#contact" class="active"><i class="fa fa-envelope"></i>Contact Me</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <section class="home active" id="home">
+                <div class="intro">
+                    <div class="row">
+                        <div class="homeinfo">
+                            <div class="animation">
+                                <div>
+                                    <h3 class="start" style="font-size: 30px;">Hello I'm a</h3>
+                                </div>
+                                <div class="second">
+                                    <li><span class="ani">Web Designer</span></li>
+                                    <li><span class="ani">Developer</span></li>
+                                    <li><span class="ani">UI Designer</span></li>
+                                    <li><span class="ani">Freelancer</span></li>
+                                </div>
+                            </div>
+                            <p class="prof">I build things related to web</p>
+                            <h4 class="aboutme">I'm a full stack web developer with lots of enthusiasm.</h4>
+                            <button href="cv.html" class="buttom">Downlod CV</button>
+                            <button href="contact.html" class="buttom">Contact me</button>
+                        </div>
+                        <div class="imgp">
+                            <img src="pratik1.png" alt="" width="70%">
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </section>
+            <section class="about section" id="about">
+                <div class="intro">
+                    <div class="row">
+                        <div class="about-con">
+                            <h2 class="about-h" id="titlea" style="color: rgb(255, 197, 120);"><span>About</span>Me</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="about-text">
+                            <h3>I'm <span style="color: rgba(89, 226, 10, 0.949);">Pratik Prakash</span> and<span style="color: rgba(89, 226, 10, 0.949);"> Web Developer</span></h3></h3>
+                            <p>Aspiring computer programmer with a BTech (Hons.) degree and proven problem-solving and troubleshooting skills. Interned with salesforce as developer. Seeking an entry-level programmer or coder position to continue expanding my knowledge of different languages and systems.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="information">
+                            <h3><span>Birthday</span> : 28/05/2001</h3>
+                            <h3><span>Religioun</span> : Hindu</h3>
+                            <h3><span>Language</span> : Hindi</h3>
+                            <h3><span>District</span> : Ramgarh</h3>
+                            <h3><span>State</span> : Jharkhand</h3>
+                            <h3><span>Phone</span> : 7209835688</h3>
+                        </div>
+                        <div class="skill">
+                            <div class="box">
+                                <span>1+</span>
+                                <h3>Intership</h3>
+                            </div>
+                            <div class="box">
+                                <span>4+</span>
+                                <h3>Certificate</h3>
+                            </div>
+                            <div class="box">
+                                <span>41+</span>
+                                <h3>GFG</h3>
+                            </div>
+                            <div class="box">
+                                <span>0+</span>
+                                <h3>Project</h3>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </section>
+            <section class="service section" id="service">
+                <div class="intro">
+                    <h3 class="title" class="titleS">Service</h3>
+                    <div class="row">
+                        <div class="boxs">
+                            <i class="fa fa-edit"></i>
+                            <h3 class="edit">Front End Development</h3>
+                            <div class="para">
+                                <p>Being a front-end web development, I have experience in building complex, innovative, responsive, and user-centric solutions.</p>
+                            </div>
+                            <a href="https://www.jovian.com/full-stack-developer-bootcamp?utm_source=google&utm_medium=cpc&utm_campaign=full_stack_job_guarantee&gclid=CjwKCAiArY2fBhB9EiwAWqHK6kbdWjY7UKPKhzGjOQWWCAf60znMu0fBUnMC9GDqYIWK2027v3wzmxoCp0QQAvD_BwE">Learn</a>
+                        </div>
+                        <div class="boxs">
+                            <i class="fa fa-code"></i>
+                            <h3 class="edit">Back End Development</h3>
+                            <div class="para">
+                                <p>Being a back-end web development,covering APIs, server-side scripting, middleware, the cloud, databases, servers, and more.</p>
+                            </div>
+                            <a href="https://www.jovian.com/full-stack-developer-bootcamp?utm_source=google&utm_medium=cpc&utm_campaign=full_stack_job_guarantee&gclid=CjwKCAiArY2fBhB9EiwAWqHK6kbdWjY7UKPKhzGjOQWWCAf60znMu0fBUnMC9GDqYIWK2027v3wzmxoCp0QQAvD_BwE">Learn</a>
+                        </div>
+                        <div class="boxs">
+                            <i class="fa fa-file-code-o" style="margin-top: -26px;"></i>
+                            <h3 class="edit">Full Stack Development</h3>
+                            <div class="para">
+                                <p>Being a full stack web developer,I can make a dynamic web sites with all responsiveness and more.</p>
+                            </div>
+                            <a href="https://www.jovian.com/full-stack-developer-bootcamp?utm_source=google&utm_medium=cpc&utm_campaign=full_stack_job_guarantee&gclid=CjwKCAiArY2fBhB9EiwAWqHK6kbdWjY7UKPKhzGjOQWWCAf60znMu0fBUnMC9GDqYIWK2027v3wzmxoCp0QQAvD_BwE">Learn</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="certifacate section" id="certifacate">
+                <h2 class="title" id="titlec">Certificate</h2>
+                <div class="slider">
+                    <i onclick="prev()" class="fa fa-angle-left" id="arro" style="font-size: 90px; position: relative; padding: 0px 0px; color: rgba(89, 226, 10, 0.949);"></i>
+                    <img class="slider-img" src="salesforce.jpeg" alt="" width="50%" style="position: relative; transition: all 0.9s ease;">
+                    <i onclick="next()" class="fa fa-angle-right" id="arro" style="font-size: 90px; position: relative; padding: 0px 0px; color: rgba(89, 226, 10, 0.949);"></i>
+                </div>
+            </section>
+            <section class="contact section" id="contact">
+                <div class="intro">
+                    <h2 class="title">Contact me</h2>
+                    <div class="contact-content">
+                        <div class="column left">
+                            <div class="text">Get in Touch</div>
+                            <p>Any issues or any quary you are free to communicate with me at any time there are many option to be in touch....</p>
+                            <div class="icons">
+                                <div class="row3">
+                                    <i class="fas fa-user"></i>
+                                    <div class="info">
+                                        <div class="head">Name</div>
+                                        <div class="sub-title">Pratik Prakashi</div>
+                                    </div>
+                                </div>
+                                <div class="row3">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                    <div class="info">
+                                        <div class="head">Address</div>
+                                        <div class="sub-title">Ramgarh,Jharkhand</div>
+                                    </div>
+                                </div>
+                                <div class="row3">
+                                    <i class="fas fa-envelope"></i>
+                                    <div class="info">
+                                        <div class="head">Email</div>
+                                        <div class="sub-title">singhpratik3696@gmail.com</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="column right">
+                            <div class="text">Message me</div>
+                            <form action="sucess.php" method="post">
+                                <div class="fields">
+                                    <div class="field name">
+                                        <input type="text"  name="name" id="name" placeholder="Name" required /><b><span class="formerror"></span></b>
+                                    </div>
+                                    <div class="field email">
+                                        <input type="email" name="email" id="email" placeholder="Email" required /><b><span class="formerror"></span></b>
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <input type="text" name="subject" id="subject" placeholder="Subject" required />
+                                </div>
+                                <div class="field textarea">
+                                    <input cols="30" rows="10"  name="comment" id="comment" placeholder="Message.." required>
+                                
+                                </div>
+                                <div class="button-area">
+                                    <button type="submit" id="submit">Send message</button>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                        if($_SERVER['REQUEST_METHOD']=='POST'){
+                            $name=$_POST['name'];
+                            $email=$_POST['email'];
+                            $subject=$_POST['subject'];
+                            $comment=$_POST['comment'];
+
+
+
+                            $servername="localhost";
+                            $username="id20272200_root";
+                            $password="Pratik@12345";
+                            $database="id20272200_portfoliodata";
+
+                            $conn=mysqli_connect($servername,$username,$password,$database);
+
+                            if(!$conn){
+                                die('sorry we failed to connect:'. mysqil_connect_error());
+                            }
+                            else{
+                                echo"connection was succesfully";
+                            
+
+                                $sql="INSERT INTO `database_table` (`Name`, `Email`, `Subject`, `Comment`) VALUES ('$name', '$email', '$subject','$comment')";
+                                $result=mysqli_query($conn,$sql);
+
+
+                                if($result){
+                                    echo"successfulll";
+                                }
+                                else{
+                                    echo"the record was not inserted succesfully becource of error....>". mysqli_error($conn);
+                                }
+                            }
+                        }
+                        ?>
+                        <div class="social">
+                            <div class="row1">
+                                <a href="https://www.facebook.com/pratik.dsingh" class="fa fa-facebook"></a>
+                                <a href="https://twitter.com/PratikP54971931" class="fa fa-twitter"></a>
+                                <a href="#" class="fa fa-google"></a>
+                                <a href="https://www.linkedin.com/in/pratik-prakash-318b6b250/" class="fa fa-linkedin"></a>
+                                <a href="https://github.com/Singhpratik1" class="fa fa-github"></a>
+                                <a href="https://www.instagram.com/pratik_singh_0521/" class="fa fa-instagram"></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    </div>
+    <footer>
+        <span>Created By <a href="https://www.codingnepalweb.com">PratikPrakash</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
+    </footer>
+    <script src="indexp.js"></script>
+</body>
+</html>
---
+++ sucess.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css1.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Poppins">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cinzel|Fauna+One">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Successfully Register</title>
+</head>
+<body>
+    <div class="up">
+        <div class="title1"><span style="background-color: #060606;"><span style="color:rgba(89, 226, 10, 0.949); background-color: #060606;">P</span>ratik <span style="color:rgba(89, 226, 10, 0.949); background-color: #060606;">P</span>rakash</span></div>
+        <div class="menu1"><i onclick="openmenu()" class="fa fa-home" style="color: rgba(89, 226, 10, 0.949);"><a href=""></a></i></div>
+    </div>
+    <div class="main">
+        <div class="box">
+            <h3 style="background-color: rgb(22, 22, 23);">Thank you!!!</h3>
+            <h2 style="background-color: rgb(19, 19, 20);">You have register successfully</h2>
+            <p style="background-color: rgb(15, 15, 15);">I will connect with you shortly.</p>
+        </div>
+    </div>
+    <div class="back">
+        <button><i class="fa fa-home" style="background-color: rgb(20, 20, 20); color: rgba(89, 226, 10, 0.949); padding: 10px;"></i><a href="">Home</a></button>
+    </div>
+</body>
+</html>
